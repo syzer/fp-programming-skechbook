e752439ec672bd20d11175fd0beab417f4324ffe
syzer3@gmail.com <syzer3@gmail.com>
2017-01-10 12:40:41 +0100

    idempotency

--- 02-curry-in-action.js
+++ 02-curry-in-action.js
@@ -42,7 +42,34 @@ const data = {
     ]
 }
 
+// X indempotent
+function testMe(a, date = new Date()) {
+    return date + a
+}
 
+// curl -X DELETE localhost:/api/users/666
+// => 200
+// => 204
+
+// => 404
+// not an error
+// TODO lets check
+
+// curl -X DELETE localhost:/api/users/666
+// curl -X DELETE localhost:/api/users/667
+
+// curl -X DELETE localhost:/api/users/666,667
+
+// curl -X DELETE localhost:/api/users/666
+// ... 204
+
+// curl -X DELETE localhost:/api/users/666
+// ... 404
+
+
+// pure
+// 1. return Object... no side effect
+// 2. pure => return Object... no side effect
 getIncompleteTaskSummaries('Lena')
     .then(console.log)
     .catch(console.error)
