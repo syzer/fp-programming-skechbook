b15b7173d0e1b85e4bc52fd60626cf822ccaee58
syzer3@gmail.com <syzer3@gmail.com>
2017-01-10 13:05:50 +0100

    curried add

--- 01-curry.js
+++ 01-curry.js
@@ -1,11 +1,18 @@
-// lets frite a function
-add(1)(2)(3)
-// =>6
-
+const _ = require('lodash')
 
 const add =
     a =>
         b =>
             c => a + b + c
 
+// lets frite a function
+add(1)(2)(3)
+// =>6
+
 console.log(add(1)(2)(3))
+
+const conventionalAdd = (a, b, c) => a + b + c
+
+const curriedAdd = _.curry(conventionalAdd)
+
+console.log(curriedAdd(1, 2)(3))
