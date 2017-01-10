830aef42f4a600a1b3fd625c5657cdcf6d281e86
munxar <sascha.aeppli@liip.ch>
2017-01-10 14:27:21 +0100

    first attemt

--- 03-function-composition.js
+++ 03-function-composition.js
@@ -1,18 +1,18 @@
 const _ = require('lodash');
 
 // Updates an existing Ib in the DB
-function patch(req, res, error) {
+function patch({req, res, error}) {
 
     return "123";
 }
 
-const onlyAdmin = (req, res) => {
+const onlyAdmin = ({req, res, error}) => {
     const error = req.user != 'dmin';
 
     return {req, res, error}
 }
 
-const removeId = (req, res, error) => {
+const removeId = ({req, res, error}) => {
     if(req.body._id) {
         delete req.body._id;
     }
@@ -21,7 +21,13 @@ const removeId = (req, res, error) => {
 
 const adminOnlyPatch = _.flow(onlyAdmin, removeId, patch);
 
-adminOnlyPatch({user: 'admin', body: {_id:'123'}}, {});
+const data = {
+    req: {user: 'admin', body: {_id:'123'}},
+    res: {},
+    error: false
+}
+
+adminOnlyPatch(data);
 
 
 /*
